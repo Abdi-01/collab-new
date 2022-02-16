255c2dd0ee1950095c8ee7392432477b3a547fbb
boblightning <satrianugraha4000@gmail.com>
2022-02-16 11:19:50 +0700

    CL-bob-01:Footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      {/* place your component here ⬇️ */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
