87b30d501a63db0fbf6f49e55dbcf7dd1ada9a27
fanotastic <fawwazputrou@gmail.com>
2022-02-16 11:15:37 +0700

    CL-fawwazputro-01: NavbarCollab

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarCollab from './components/Navbar';
+function App() {
+  return (
+    <div>
+      {/* place your component here ⬇️ */}
+      <NavbarCollab/>
+    </div>
+  );
+}
+
+export default App;
