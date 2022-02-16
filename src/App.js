81bf6421577fefbf30b6dead5484de88d9b8394c
reyhanbalthazar <93903964+reyhanbalthazar@users.noreply.github.com>
2022-02-16 11:20:11 +0700

    CL-reyhan-01:FormEmail

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
+function App() {
+  return (
+    <div>
+      <Form/>
+      {/* place your component here ⬇️ */}
+    </div>
+  );
+}
+
+export default App;
